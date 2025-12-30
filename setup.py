d23712fb859b7f52906431a8f5a838038b47f0f4
AryanDhanuka10 <a9936067905@gmail.com>
2025-12-31 00:13:27 +0530

    Add setup.py for modular package installation

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Credit_Risk_Modelling",
+    version="0.0.1",
+    author="Aryan",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+)
